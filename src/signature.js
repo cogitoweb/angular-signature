2aa7851b92e8ad165e2371441c126e4ea180f8be
Paolo Conizzoli <paolo@conizzoli.com>
2018-05-07 14:31:39 +0200

    Revert "modifica plugin per funzionamento su GOV"
    This reverts commit 67245bc1875bbefd04174d47e8f4ba0371ba30ea.

--- src/signature.js
+++ src/signature.js
@@ -6,96 +6,94 @@
 angular.module('signature', []);
 
 angular.module('signature').directive('signaturePad', ['$window', '$timeout',
-	function ($window, $timeout) {
-		'use strict';
+  function ($window, $timeout) {
+    'use strict';
 
-		var signaturePad, canvas, element, EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=';
-		return {
-			restrict: 'EA',
-			replace: true,
-			template: '<div class="signature" ng-style="{height: height + \'px\', width: width + \'px\'}"><canvas ng-mouseup="updateModel()" style="width: 70vw !important; height: 60vh !important;"></canvas></div>',
-			scope: {
-				accept: '=',
-				clear: '=',
-				dataurl: '=',
-				signature: '=',
-				height: '@',
-				width: '@'
-			},
-			controller: [
-				'$scope',
-				function ($scope) {
-					$scope.accept = function () {
-						var signature = {};
+    var signaturePad, canvas, element, EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=';
+    return {
+      restrict: 'EA',
+      replace: true,
+      template: '<div class="signature" ng-style="{height: height + \'px\', width: width + \'px\'}"><canvas ng-mouseup="updateModel()"></canvas></div>',
+      scope: {
+        accept: '=',
+        clear: '=',
+        dataurl: '=',
+        height: '@',
+        width: '@'
+      },
+      controller: [
+        '$scope',
+        function ($scope) {
+          $scope.accept = function () {
+            var signature = {};
 
-						if (!$scope.signaturePad.isEmpty()) {
-							signature.dataUrl = $scope.signaturePad.toDataURL();
-							signature.isEmpty = false;
-						} else {
-							signature.dataUrl = EMPTY_IMAGE;
-							signature.isEmpty = true;
-						}
+            if (!$scope.signaturePad.isEmpty()) {
+              signature.dataUrl = $scope.signaturePad.toDataURL();
+              signature.isEmpty = false;
+            } else {
+              signature.dataUrl = EMPTY_IMAGE;
+              signature.isEmpty = true;
+            }
 
-						$scope.signature = signature;
-						return signature;
-					};
+            return signature;
+          };
 
-					$scope.updateModel = function () {
-						/*
-						 defer handling mouseup event until $scope.signaturePad handles
-						 first the same event
-						 */
-						$timeout()
-							.then(function () {
-								var result = $scope.accept();
-								$scope.dataurl = result.isEmpty ? undefined : result.dataUrl;
-							});
-					};
+          $scope.updateModel = function () {
+            /*
+             defer handling mouseup event until $scope.signaturePad handles
+             first the same event
+             */
+            $timeout()
+              .then(function () {
+                var result = $scope.accept();
+                $scope.dataurl = result.isEmpty ? undefined : result.dataUrl;
+              });
+          };
 
-					$scope.clear = function () {
-						$scope.signaturePad.clear();
-						$scope.dataurl = undefined;
-					};
+          $scope.clear = function () {
+            $scope.signaturePad.clear();
+            $scope.dataurl = undefined;
+          };
 
-					$scope.$watch("dataurl", function (dataUrl) {
-						if (dataUrl) {
-							$scope.signaturePad.fromDataURL(dataUrl);
-						}
-					});
-				}
-			],
-			link: function (scope, element) {
-				canvas = element.find('canvas')[0];
-				scope.signaturePad = new SignaturePad(canvas);
+          $scope.$watch("dataurl", function (dataUrl) {
+            if (dataUrl) {
+              $scope.signaturePad.fromDataURL(dataUrl);
+            }
+          });
+        }
+      ],
+      link: function (scope, element) {
+        canvas = element.find('canvas')[0];
+        scope.signaturePad = new SignaturePad(canvas);
 
-				if (scope.signature && !scope.signature.$isEmpty && scope.signature.dataUrl) {
-					scope.signaturePad.fromDataURL(scope.signature.dataUrl);
-				}
+        if (scope.signature && !scope.signature.$isEmpty && scope.signature.dataUrl) {
+          scope.signaturePad.fromDataURL(scope.signature.dataUrl);
+        }
 
-				scope.onResize = function() {
-					var canvas = element.find('canvas')[0];
-					var ratio =  Math.max($window.devicePixelRatio || 1, 1);
-					canvas.width = canvas.offsetWidth * ratio;
-					canvas.height = canvas.offsetHeight * ratio;
-					canvas.getContext("2d").scale(ratio, ratio);
+        scope.onResize = function() {
+          var canvas = element.find('canvas')[0];
+          var ratio =  Math.max($window.devicePixelRatio || 1, 1);
+          canvas.width = canvas.offsetWidth * ratio;
+          canvas.height = canvas.offsetHeight * ratio;
+          canvas.getContext("2d").scale(ratio, ratio);
 
-					// reset dataurl
-					scope.dataurl = null;
-				}
+          // reset dataurl
+          scope.dataurl = null;
+        }
 
-				scope.onResize();
+        scope.onResize();
 
-				angular.element($window).bind('resize', function() {
-						scope.onResize();
-				});
+        angular.element($window).bind('resize', function() {
+            scope.onResize();
+        });
 
-				// listen to touchend event and updateModel when event is fired
-				element.on('touchend', function () {
-					scope.$apply(scope.updateModel);
-				});
-			}
-		};
-	}
+        // listen to touchend event and updateModel when event is fired
+        element.on('touchend', function () {
+            scope.$apply(scope.updateModel);
+        });
+      }
+    };
+  }
 ]);
 
 // Backward compatibility
